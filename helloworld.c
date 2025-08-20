5e769fcc91d605d812eb9997e728a50f33155f42
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-08-20 18:00:52 -0300

    Menu_Calculadora de Força adicionado

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main(){
+void StrengthCalculator () { //Função 1 - Calculadora de Força
+    int peso;
+    int reps;
+
+    system("clear");
+    printf("╔══════════════════════════════════════════════╗\n");
+    printf("║          MENU_CALCULADORA DE FORÇA           ║\n");
+    printf("╚══════════════════════════════════════════════╝\n");
+    printf("➤ Carga: \n");
+    scanf("%d", &peso);
+    printf("➤ Repetições: \n");
+    scanf("%d", &reps);
+}
+
+int main(){ //Função principal - Tela inicial
+    int opcao;
+    
+    system("clear"); //Comando - Limpar tela inicial
     printf("╔══════════════════════════════════════════════════════════════════════════╗\n");
     printf("║  ██╗  ██████╗    ██████╗   ███╗   ██╗     ██╗  ██╗  ██╗   ██╗  ██████╗   ║\n");
     printf("║  ██║  ██╔══██╗  ██╔═══██╗  ███╗   ██║     ██║  ██║  ██║   ██║  ██╔══██╗  ║\n");
@@ -9,6 +26,14 @@ int main(){
     printf("║  ██║  ██║  ██║  ╚██████╔╝  ██║ ╚████║     ██║  ██║  ╚██████╔╝  ██████╔╝  ║\n");
     printf("║  ╚═╝  ╚═╝  ╚═╝   ╚═════╝   ╚═╝  ╚═══╝     ╚═╝  ╚═╝   ╚═════╝   ╚═════╝   ║\n");
     printf("╚══════════════════════════════════════════════════════════════════════════╝\n");
+    printf("                       1 . CALCULADORA DE FORÇA                             \n");
+    printf("════════════════════════════════════════════════════════════════════════════\n");
+    printf("➤ Escolha uma opção: ");
+    scanf("%d", &opcao);
+
+    if(opcao == 1) {
+        StrengthCalculator(); //Modulo StregthCalculator
+    }
 
     return 0;
 }
